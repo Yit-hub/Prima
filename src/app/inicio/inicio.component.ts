ced68a277c45b9e7af967f1655a0c002c400afed
Yit-hub <yidanielchen.23@campuscamara.es>
2024-12-02 13:03:26 +0100

    d

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.scss']
 })
-export class InicioComponent {
+export class InicioComponent implements OnInit{
+  images = [
+    'https://prima.es/wp-content/themes/prima/assets/images/ket.png',
+    'https://prima.es/wp-content/themes/prima/assets/images/mos.png',
+    'https://prima.es/wp-content/themes/prima/assets/images/may.png'
+  ];
+  colors = ['#FF0000', '#0047AB', '#f0c424'];
+  currentIndex = 0;
 
+  ngOnInit(): void {
+    setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.images.length;
+    }, 3000); // Cambiar cada 3 segundos
+  }
 }
